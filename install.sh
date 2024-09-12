7593bd4647b0543f677084592477c59718d10b22
LeeXiongfei <lxf@leefei.cn>
2024-09-13 02:40:21 +0800

    添加批量执行文件

---
+++ install.sh
@@ -0,0 +1,70 @@
+cd cat1/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat2/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat3/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat4/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat5/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat6/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat7/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat8/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat9/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
+cd cat10/
+# 执行 yarn install
+yarn install
+# 执行 yarn build
+yarn build
+# 返回上一层目录
+cd ../
